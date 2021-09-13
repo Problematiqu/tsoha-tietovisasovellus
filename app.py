56207496d68d54d21c74b0b91c1c2032401fb564
Sami Kangas <herttua.sami@gmail.com>
2021-09-13 13:29:36 +0300

    Added templates

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask 
-from flask import render_template 
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", test="Example")
+    return render_template("index.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
