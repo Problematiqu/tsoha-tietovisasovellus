231e473f070089d54d4a03ab789cdd6d3b11272e
Sami Kangas <herttua.sami@gmail.com>
2021-10-10 14:42:26 +0300

    Pylint corrections

--- routes.py
+++ routes.py
@@ -1,8 +1,8 @@
-from app import app 
+from app import app
 from flask import render_template, request, redirect, session, flash
 from werkzeug.security import check_password_hash, generate_password_hash
-from random import randint, shuffle
 from forms import AddForm, QuizForm, RegisterForm, LoginForm
+from random import shuffle
 import sql_commands
 
 @app.route("/")
@@ -13,11 +13,10 @@ def index():
 def start():
     return render_template("start.html")
 
-@app.route("/quiz", methods=["GET", "POST"]) 
+@app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-    
     if "user_id" in session:
-        remaining_questions = sql_commands.get_quiz(session["user_id"]) 
+        remaining_questions = sql_commands.get_quiz(session["user_id"])
 
     if "user_id" in session and remaining_questions:
         question_ids = [question.question for question in remaining_questions]
@@ -30,7 +29,7 @@ def quiz():
         shuffle(question_ids)
         question_ids = question_ids[0:10]
 
-    questions = {}    
+    questions = {}
     for question_id in question_ids:
         options = sql_commands.get_options(question_id)
         shuffle(options)
@@ -47,14 +46,16 @@ def quiz():
     i = 0
     for question in questions:
         form.question[i].label = questions[question][0].question
-        form.question[i].choices = [(questions[question][0].id, questions[question][0].option), (questions[question][1].id, questions[question][1].option), \
-            (questions[question][2].id, questions[question][2].option), (questions[question][3].id, questions[question][3].option)]
+        form.question[i].choices = [
+            (questions[question][0].id, questions[question][0].option),
+            (questions[question][1].id, questions[question][1].option),
+            (questions[question][2].id, questions[question][2].option),
+            (questions[question][3].id, questions[question][3].option)]
         i += 1
 
     if form.validate_on_submit():
         data = form.question.data
         score = 0
-    
         for value in data:
             correct = sql_commands.is_correct(value)
             if correct:
@@ -67,38 +68,12 @@ def quiz():
             del session["questions"]
 
         return render_template("finish.html", score=score)
-    
     return render_template("quiz.html", form=form)
-
-@app.route("/result", methods=["POST"])
-def result():
-    form = QuizForm(request.form)
-    print(form.question.data)
-    if form.validate_on_submit():
-        results = request.form
-        print(form.question.data)
-        score = 0
-    
-        for key in results.keys():
-            value = results.get(key)
-            correct = sql_commands.is_correct(value)
-            if correct:
-                score += 1
-            if "user_id" in session:
-                sql_commands.add_answer(value, session["user_id"])
-    
-        if "user_id" in session:
-            sql_commands.delete_quiz(session["user_id"])
-    
-        return render_template("finish.html", score=score)
-
-    print(form.errors)
-    return redirect("/quiz")
 
 @app.route("/scoreboard")
 def scoreboard():
-    scoreboard = sql_commands.get_scoreboard()   
-    return render_template("scoreboard.html", scoreboard=scoreboard)
+    scores = sql_commands.get_scoreboard()
+    return render_template("scoreboard.html", scoreboard=scores)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -109,23 +84,22 @@ def login():
         form = LoginForm()
     else:
         form = LoginForm(request.form)
-        if form.validate_on_submit():      
+        if form.validate_on_submit():
             user = sql_commands.get_user(form.username.data)
             if not user:
                 flash("Käyttäjätunnus tai salasana olivat vääriä", "danger")
                 return redirect("/login")
-            else:
-                hash_value = user.password
-                if check_password_hash(hash_value, form.password.data):
-                    session["user_id"] = user.id
-                    session["admin"] = sql_commands.get_admin(user.id)
-                    if "questions" in session: 
-                        del session["questions"]
-                    flash(f"Kirjauduttu sisään käyttäjänä {form.username.data}", "success")
-                    return redirect("/")
-                else:
-                    flash("Käyttäjätunnus tai salasana olivat vääriä", "danger")
-                    return redirect("/login")
+            hash_value = user.password
+            if check_password_hash(hash_value, form.password.data):
+                session["user_id"] = user.id
+                session["admin"] = sql_commands.get_admin(user.id)
+                if "questions" in session:
+                    del session["questions"]
+                flash(f"Kirjauduttu sisään käyttäjänä {form.username.data}", "success")
+                return redirect("/")
+
+            flash("Käyttäjätunnus tai salasana olivat vääriä", "danger")
+            return redirect("/login")
 
     return render_template("login.html", form=form)
 
@@ -144,21 +118,18 @@ def register():
         form = RegisterForm()
     else:
         form = RegisterForm(request.form)
-        if form.validate_on_submit():      
+        if form.validate_on_submit():
             user = sql_commands.get_user(form.username.data)
-    
             if user:
                 flash("Käyttäjänimi on jo käytössä", "danger")
                 return render_template("register.html", form=form)
-    
             hash_value = generate_password_hash(form.password.data)
             sql_commands.add_user(form.username.data, hash_value)
             flash(f"Käyttäjätunnus luotu käyttäjälle {form.username.data}", "success")
-    
             return redirect("/")
 
     return render_template("register.html", form=form)
-        
+
 @app.route("/profile")
 def profile():
     if "user_id" not in session:
@@ -169,22 +140,22 @@ def profile():
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    if "user_id" not in session: 
+    if "user_id" not in session:
         return redirect("/")
-    
+
     admin = sql_commands.get_admin(session["user_id"])
 
     if not admin:
         return redirect("/")
 
-    if request.method =="GET":
+    if request.method == "GET":
         form = AddForm()
     else:
         form = AddForm(request.form)
         if form.validate_on_submit():
-            sql_commands.add_question(form.question.data, int(form.topic.data), form.correct.data, form.incorrect.data)
+            sql_commands.add_question(form.question.data, int(form.topic.data),
+                                      form.correct.data, form.incorrect.data)
             flash(f"Kysymys '{form.question.data}' lisätty!", "success")
             return redirect("/add")
-            
-    return render_template("add.html", form=form)
 
+    return render_template("add.html", form=form)
--- sql_commands.py
+++ sql_commands.py
@@ -1,8 +1,8 @@
 from db import db
 
-def add_answer(id: int, user: int):
+def add_answer(option_id: int, user: int):
     sql = "INSERT INTO answers (option_id, user_id) VALUES (:option, :user)"
-    db.session.execute(sql, {"option": id, "user": user})
+    db.session.execute(sql, {"option": option_id, "user": user})
     db.session.commit()
 
 def add_user(username: str, hash_value):
@@ -13,32 +13,33 @@ def add_user(username: str, hash_value):
 def add_question(question: str, topic: int, correct_option: str, options: list):
     sql = "INSERT INTO questions (question, topic_id) VALUES (:question, :topic) RETURNING id"
     query_result = db.session.execute(sql, {"question": question, "topic": topic})
-    id = query_result.fetchone()[0]
+    question_id = query_result.fetchone()[0]
     sql = "INSERT INTO options (option, correct, question_id) VALUES (:option, TRUE, :id)"
-    db.session.execute(sql, {"option": correct_option, "id": id})
+    db.session.execute(sql, {"option": correct_option, "id": question_id})
     for option in options:
         sql = "INSERT INTO options (option, correct, question_id) VALUES (:option, FALSE, :id)"
-        db.session.execute(sql, {"option": option, "id": id})
+        db.session.execute(sql, {"option": option, "id": question_id})
     db.session.commit()
 
-def add_quiz(id: int, quiz: int):
+def add_quiz(user_id: int, quiz: int):
     sql = "INSERT INTO quiz (user_id, question) VALUES (:id, :quiz)"
-    db.session.execute(sql, {"id": id, "quiz": quiz})
+    db.session.execute(sql, {"id": user_id, "quiz": quiz})
     db.session.commit()
 
-def delete_quiz(id: int):
+def delete_quiz(user_id: int):
     sql = "DELETE FROM quiz WHERE user_id=:id"
-    db.session.execute(sql, {"id": id})
+    db.session.execute(sql, {"id": user_id})
     db.session.commit()
 
-def get_admin(id: int):
+def get_admin(user_id: int):
     sql = "SELECT admin FROM users WHERE id=:id"
-    query_result = db.session.execute(sql, {"id": id})
+    query_result = db.session.execute(sql, {"id": user_id})
     return query_result.fetchone()[0]
 
 def get_options(number: int):
-    sql = "SELECT Q.question, O.option, O.id FROM questions Q LEFT JOIN options O ON Q.id = O.question_id WHERE Q.id=:number"
-    query_result = db.session.execute(sql, {"number": number}) 
+    sql = "SELECT Q.question, O.option, O.id FROM questions Q " \
+          "LEFT JOIN options O ON Q.id = O.question_id WHERE Q.id=:number"
+    query_result = db.session.execute(sql, {"number": number})
     return query_result.fetchall()
 
 def get_question_ids():
@@ -48,17 +49,19 @@ def get_question_ids():
 
 def get_scoreboard():
     sql = "SELECT U.username, CEILING((CAST(COUNT(*) FILTER (where O.correct) AS FLOAT) / COUNT(*)) * 100) AS correct_percentage, COUNT(*) AS total " \
-          "FROM  answers A INNER JOIN options O ON A.option_id = O.id LEFT JOIN users U ON U.id = A.user_id " \
+          "FROM answers A " \
+          "INNER JOIN options O ON A.option_id = O.id " \
+          "LEFT JOIN users U ON U.id = A.user_id " \
           "GROUP BY U.username ORDER BY correct_percentage DESC, total LIMIT 20"
     query_result = db.session.execute(sql)
-    return query_result.fetchall() 
+    return query_result.fetchall()
 
 def get_user(username: str):
     sql = "SELECT id, password FROM users WHERE username=:username"
     query_result = db.session.execute(sql, {"username": username})
     return query_result.fetchone()
 
-def get_user_scores(id: int):
+def get_user_scores(user_id: int):
     sql = "SELECT T.id, T.topic, COUNT(A.user_id) AS total, COUNT(*) FILTER (where O.correct) AS correct " \
           "FROM questions Q " \
           "INNER JOIN options O ON O.question_id = Q.id  " \
@@ -66,12 +69,12 @@ def get_user_scores(id: int):
           "RIGHT JOIN topic T ON T.id = Q.topic_id " \
           "GROUP BY T.id " \
           "ORDER BY T.id"
-    query_result = db.session.execute(sql, {"user_id": id})
+    query_result = db.session.execute(sql, {"user_id": user_id})
     return query_result.fetchall()
 
-def get_quiz(id: int):
+def get_quiz(user_id: int):
     sql = "SELECT question FROM quiz WHERE user_id=:id"
-    query_result = db.session.execute(sql, {"id": id})
+    query_result = db.session.execute(sql, {"id": user_id})
     return query_result.fetchall()
 
 def get_topics():
@@ -79,24 +82,12 @@ def get_topics():
     query_result = db.session.execute(sql)
     return query_result.fetchall()
 
-def is_correct(id: int):
+def is_correct(option_id: int):
     sql = "SELECT correct FROM options WHERE id=:value"
-    query_result = db.session.execute(sql, {"value": id})
+    query_result = db.session.execute(sql, {"value": option_id})
     return query_result.fetchone()[0]
 
 def question_count():
     sql = "SELECT COUNT(id) FROM questions"
     query_result = db.session.execute(sql)
     return query_result.fetchone()[0]
-
-
-
-
-
-
-
-
-
-
-
-
